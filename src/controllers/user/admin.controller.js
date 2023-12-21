90a24c4b34748f5f9488b96905a66dc2bc971dd3
Harsh <harsh105savaliya@gmail.com>
2023-12-21 11:44:14 +0530

    Login API

--- src/controllers/user/admin.controller.js
+++ src/controllers/user/admin.controller.js
@@ -1,12 +1,13 @@
 import Validator from "validatorjs";
 import RESPONSE from "../../helpers/response.js";
 import { User } from "../../models/users.model.js";
+import bcrypt from "bcrypt";
 
 const AdminCreate = async (req, res) => {
   // validating the fields
   const validation = new Validator(req.body, {
-    username: "required",
-    email: "required|email",
+    username: ["required", "string", "regex:/^[a-z0-9]+$/"],
+    email: "required|string|email",
     contact_no: "required|digits:10",
     password: "required",
     role: "required|in:admin,user",
@@ -74,16 +75,85 @@ const AdminCreate = async (req, res) => {
   }
 };
 
+const AdminLogin = async (req, res) => {
+  // validating the fields
+  const validation = new Validator(req.body, {
+    username: ["required", "string", "min:6"],
+    password: "required",
+  });
+
+  // returning error on validation fail
+  if (validation.fails()) {
+    const errorMessage = Object.keys(validation.errors.all())[0];
+    return RESPONSE.error(res, validation.errors.first(errorMessage));
+  }
+
+  try {
+    const {
+      body: { username, password },
+    } = req;
+
+    let condition;
+    if (username.trim() && !isNaN(username)) {
+      condition = {
+        $or: [{ contact_no: username }],
+      };
+    } else {
+      condition = {
+        $or: [{ username: username }, { email: username }],
+      };
+    }
+    const findUser = await User.findOne(condition);
+
+    if (!findUser) {
+      return RESPONSE.error(res, "User doesn't exists", 404);
+    }
+
+    const comparePassword = await bcrypt.compare(password, findUser.password);
+
+    if (!comparePassword) {
+      return RESPONSE.error(res, "Invalid credentials", 403);
+    }
+
+    // generating token
+    const generateToken = await findUser.generateAccessToken();
+
+    // updating token in users data
+    const tokenUpdatedRes = await User.findByIdAndUpdate(
+      {
+        _id: findUser._id,
+      },
+      {
+        $set: { token: generateToken },
+      }
+    );
+
+    const findUpdatedUser = await User.findById(tokenUpdatedRes._id).select(
+      "-__v -createdAt -updatedAt"
+    );
+
+    return RESPONSE.success(res, "Logged in successfully", findUpdatedUser);
+  } catch (error) {
+    // sending error response
+    return RESPONSE.error(res, 9999, 500, error);
+  }
+};
+
 const AdminLogout = async (req, res) => {
-  //
+  // userdetail from auth middleware
   const verifiedUser = req.verifiedUser;
   try {
+    // removing token by updating user
     const updateUser = await User.findByIdAndUpdate(verifiedUser._id, {
       $set: { token: null },
     });
+
+    // finding the updated user and filtering the required fields
     const findUpdatedUser = await User.findById(updateUser._id).select(
       "-__v -createdAt -updatedAt"
     );
+
+    // returning final response
     return RESPONSE.success(res, "Logout successfully", findUpdatedUser);
   } catch (error) {
     // sending error response
@@ -91,4 +161,4 @@ const AdminLogout = async (req, res) => {
   }
 };
 
-export { AdminCreate, AdminLogout };
+export { AdminCreate, AdminLogout, AdminLogin };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   AdminCreate,
+  AdminLogin,
   AdminLogout,
 } from "../controllers/user/admin.controller.js";
 import { auth } from "../middleware/Auth.middleware.js";
@@ -9,5 +10,6 @@ const router = Router();
 
 router.route("/create").post(AdminCreate);
 router.route("/logout").post(auth, AdminLogout);
+router.route("/login").post(AdminLogin);
 
 export default router;
